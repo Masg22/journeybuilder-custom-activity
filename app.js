f0137d3d48b1fcd3d3504fa492f77e4f966f883b
Miguel Angel Serrat Gil <62011560+Masg22@users.noreply.github.com>
2022-01-26 10:15:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -63,4 +63,9 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
