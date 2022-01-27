e2b9b60b3964817b87e883bf24454e163110c847
Miguel Angel Serrat Gil <62011560+Masg22@users.noreply.github.com>
2022-01-27 10:28:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ app.use(
   }),
 );
 
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+    if (req.method === "OPTIONS") res.send(200);
+    else next();
+}
+app.use(allowCrossDomain);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
